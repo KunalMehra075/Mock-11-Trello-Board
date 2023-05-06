b4020ba1de1498ed6cac264aeef984d469fec783
KunalMehra075 <112753481+KunalMehra075@users.noreply.github.com>
2023-05-06 15:34:14 +0530

    Loading_Added

--- frontend/src/Components/Loading.jsx
+++ frontend/src/Components/Loading.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="FullHeight">
+      <div className="LoadingBox">
+        <img src="Images/loading.gif" alt="loadinggif" />
+        <h1>Loading...</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
